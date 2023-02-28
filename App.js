2f05a15194f985a95aad9c0f81d3b1faed8ee246
Pittawat Kittiprapas <pittawat.kittiprapas@krungsrinimble.com>
2023-02-28 21:24:47 +0700

    intial project

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import landing from './src/pages/index/landing';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!yoyoyoyoo</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="index">
+        <Stack.Screen name="index" component={landing} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
